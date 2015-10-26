f75d2d068f14bdaffcc944d5e023376b71f23e2f
Isaque Alves <isaquealves@gmail.com>
2015-10-26 19:19:24 -0300

    Add script that should be used to set environment variables needed to work with the client class

---
+++ envvars.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#Use this script to properly set your environment vars
+
+export BACKAND_USERNAME=''
+export BACKAND_PASSWORD=''
+export BACKAND_APPNAME=''
+export BACKAND_ANONYMOUS_TOKEN=''
